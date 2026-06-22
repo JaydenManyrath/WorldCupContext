9327a397d872c7f986b061c57e1115d640fcddb4
Andrew Manoni <admanoni2@gmail.com>
2026-06-22 20:53:05 +0000

    API Key Setup

---
+++ world-cup.py
@@ -0,0 +1,9 @@
+import os
+
+football_key = os.getenv('FOOTBALLAPI')
+youtube_key = os.getenv('YOUTUBEAPI')
+google_key = os.getenv('GOOGLEAPI')
+
+print(football_key)
+print(youtube_key)
+print(google_key)
